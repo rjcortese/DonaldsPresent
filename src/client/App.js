6b6f2bfdedb832152ba2a6a2a673d7cf0bf690c8
R James Cortese <rjcortese@gmail.com>
2018-06-20 11:56:23 -0400

    0.04

--- src/client/App.js
+++ src/client/App.js
@@ -4,20 +4,25 @@ import "./App.css";
 class App extends Component{
     constructor(props) {
         super(props);
-        this.state = { username: null };
+        this.state = { magicword : null };
     }
 
     componentDidMount() {
-        fetch('/')
-        .then(res => res.json())
-        .then(user => this.setState({username: user.username}));
+        fetch('/woof')
+        .then((response) => {
+            return response.json();
+        })
+        .then((myJson) => {
+            console.log(myJson);
+            this.setState({magicword : myJson.magicword})
+        })
     }
 
     render() {
         return(
             <div className="App">
-                {this.state.username ? (
-                    <h1>Hello {this.state.username}</h1>
+                {this.state.magicword ? (
+                    <h1>Hello {this.state.magicword}</h1>
                 ) : (
                     <h1>Loading!</h1>
                 )}
--- src/server/index.js
+++ src/server/index.js
@@ -5,6 +5,6 @@ const app = express();
 
 app.use(express.static('dist'));
 
-app.get('/', (req, res) => res.send({ username: 'woof' }));
+app.get('/woof', (req, res) => res.send({ magicword: 'woof' }));
 
-app.listen(3232, () => console.log('Listening on port 3232!'));
+app.listen(3232, () => console.log('Listening on port 3232!!'));
