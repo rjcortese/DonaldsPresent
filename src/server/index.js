85d3d31030c82a930ec9d08e2a2671230ae7aaf9
R James Cortese <rjcortese@gmail.com>
2018-06-21 14:01:37 -0400

    0.5

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
@@ -6,5 +7,7 @@ const app = express();
 app.use(express.static('dist'));
 
 app.get('/woof', (req, res) => res.send({ magicword: 'woof' }));
+
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'public/index.html')));
 
 app.listen(3232, () => console.log('Listening on port 3232!!'));
