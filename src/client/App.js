9b54ca33e9963e9323eca241dd383a5c1f39bca0
R James Cortese <rjcortese@gmail.com>
2018-06-22 15:31:24 -0400

    0.6

--- src/client/App.js
+++ src/client/App.js
@@ -1,14 +1,78 @@
 import React, { Component } from "react";
 import "./App.css";
 
+
+class TitleBanner extends Component{
+    render() {
+        return(
+            <div className="TitleBanner">
+                <h1>"Donald's Present"</h1>
+            </div>
+        );
+    }
+}
+
+class Panel extends Component{
+    render() {
+        return(
+            <div className="Panel">
+                {this.props.value}
+            </div>
+        );
+    }
+}
+
+class GridTopRow extends Component{
+    render() {
+        return(
+            <div className="GridTopRow">
+            <Panel value=""/>
+            <Panel value="Kraken"/>
+            <Panel value="Bittrex"/>
+            <Panel value="Binance"/>
+            <Panel value="CoinCap"/>
+            <Panel value="Poloniex"/>
+            </div>
+        );
+    }
+}
+
+class GridRow extends Component{
+    render(){
+        return(
+            <div className="GridRow">
+            <Panel value={this.props.value}/>
+            <Panel value="kraken price"/>
+            <Panel value="bittrex price"/>
+            <Panel value="binance price"/>
+            <Panel value="coincap price"/>
+            <Panel value="poloniex price"/>
+            </div>
+        );
+    }
+}
+
+class Grid extends Component {
+    render(){
+        return(
+            <div className="Grid">
+            <GridTopRow/>
+            <GridRow value="Huey (Ethereum)"/>
+            <GridRow value="Duey (DASH)"/>
+            <GridRow value="Luey (Litecoin)"/>
+            </div>
+        );
+    }
+}
+
 class App extends Component{
     constructor(props) {
         super(props);
         this.state = { magicword : null };
     }
 
-    componentDidMount() {
-        fetch('/woof')
+/*    componentDidMount() {
+        fetch('/huey')
         .then((response) => {
             return response.json();
         })
@@ -17,15 +81,12 @@ class App extends Component{
             this.setState({magicword : myJson.magicword})
         })
     }
-
+*/
     render() {
         return(
             <div className="App">
-                {this.state.magicword ? (
-                    <h1>Hello {this.state.magicword}</h1>
-                ) : (
-                    <h1>Loading!</h1>
-                )}
+            <TitleBanner/>
+            <Grid/>
             </div>
         );
     }
--- src/server/index.js
+++ src/server/index.js
@@ -1,39 +1,221 @@
 const path = require('path');
 const express = require('express');
+const fetch = require('node-fetch');
 const pgp = require('pg-promise')();
+const db = pgp.('postgres://postgres:duckingit@localhost:5432/duckdb');
+
+db.any("CREATE TABLE IF NOT EXISTS exchange_data (exchange varchar(30), symbol varchar(10), rate real, timestamp timestamp);")
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 const app = express();
 
-let pullExchangeData = function() {
-    fetch('https://api.kraken.com')
+//cache to keep track of most recent rates for each currency for each exchange
+//the client api calls will get results from this instead of the db
+let exchangeRateCache = { 
+    kraken: {
+        dash: null,
+        eth: null,
+        ltc: null
+    },
+    bittrex: {
+        dash: null,
+        eth: null,
+        ltc: null
+    },
+    binanace: {
+        dash: null,
+        eth: null,
+        ltc: null
+    },
+    coincap: {
+        dash: null,
+        eth: null,
+        ltc: null
+    },
+    poloniex: {
+        dash: null,
+        eth: null,
+        ltc: null
+    }
+};
+
+function fetchKrakenRates(db, cache) {
+    fetch('https://api.kraken.com/0/public/Ticker?pair=ETHXBT,DASHXBT,LTCXBT')
+        .then((res) => {
+            return res.json();
+        }
+        .then((myjson) => {
+            //cache
+            cache.kraken.dash = myjson.result.DASHXBT.a[0];
+            cache.kraken.eth = myjson.result.XETHXXBT.a[0];
+            cache.kraken.ltc = myjson.result.XLTCXXBT.a[0];
+            //store in db
+        }
 }
 
-setInterval(pullExchangeData,1000);
+function fetchBittrexRates(db, cache) {
+    fetch('https://bittrex.com/api/v1.1/public/getticker?market=BTC-ETH')
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(myjson) {
+            //cache
+            cache.bittrex.eth = myjson.result.Ask;
+            //store in db
+        })
+        .catch(err => console.log(err));
+
+    fetch('https://bittrex.com/api/v1.1/public/getticker?market=BTC-LTC')
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(myjson) {
+            //cache
+            cache.bittrex.ltc = myjson.result.Ask;
+            //store in db
+        })
+        .catch(err => console.log(err));
+
+    fetch('https://bittrex.com/api/v1.1/public/getticker?market=BTC-DASH')
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(myjson) {
+            //cache
+            cache.bittrex.dash = myjson.result.Ask;
+            //store in db
+        })
+        .catch(err => console.log(err));
+}
+
+function fetchBinanceRates(db, cache) {
+    fetch('https://api.binance.com/api/v3/ticker/price')
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(myjson) {
+            for (let i = 0; i < myjson.length; i++) {
+                if (myjson[i].symbol === 'ETHBTC') {
+                    //cache
+                    cache.binance.eth = myjson[i].price;
+                    //store in db
+                }
+                else if (myjson[i].symbol === 'DASHBTC') {
+                    //cache
+                    cache.binance.dash = myjson[i].price;
+                    //store in db
+                }
+                else if (myjson[i].symbol === 'LTCBTC') {
+                    //cache
+                    cache.binance.ltc = myjson[i].price;
+                    //store in db
+                }
+            }
+        })
+        .catch(err => console.log(err));
+}
+
+function fetchCoincapRates(db, cache) {
+    fetch('https://coincap.io/page/ETH')
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(myjson) {
+            //cache
+            cache.coincap.eth = myjson.price_btc;
+            //store in db
+        })
+        .catch(err => console.log(err));
+
+    fetch('https://coincap.io/page/DASH')
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(myjson) {
+            //cache
+            cache.coincap.dash = myjson.price_btc;
+            //store in db
+        })
+        .catch(err => console.log(err));
+
+    fetch('https://coincap.io/page/LTC')
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(myjson) {
+            //cache
+            cap.coincap.ltc = myjson.price_btc;
+            //store in db
+        })
+        .catch(err => console.log(err));
+}
+
+function fetchPoloniexRates(db, cache) {
+    fetch('https://poloniex.com/public?command=returnTicker')
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(myjson) {
+            //cache
+            cache.poloniex.dash = myjson.BTC_DASH.lowestAsk;
+            cache.poloniex.eth = myjson.BTC_ETH.lowestAsk;
+            cache.poloniex.ltc = myjson.BTC_LTC.lowestAsk;
+            //store in db
+        })
+        .catch(err => console.log(err));
+}
+
+let fetchExchangeRates = function(db, cache) {
+    fetchKrakenRates(db, cache);
+    fetchBittrexRates(db, cache);
+    fetchBinanceRates(db, cache);
+    fetchCoincapRates(db, cache);
+    fetchPoloniexRates(db, cache);
+}
+
+setInterval(fetchExchangeRates(db, exchangeRateCache), 1500);
 
 app.use(express.static('dist'));
 
+function getMinRate(cache, sym) {
+    let min = cache.kraken[sym];
+    if ( min > cache.bittrex[sym] ) {
+        min = cache.bittrex[sym];
+    }
+    if ( min > cache.binance[sym] ) {
+        min = cache.binance[sym];
+    }
+    if ( min > cache.coincap[sym] ) {
+        min = cache.coincap[sym];
+    }
+    if ( min > cache.poloniex[sym] ) {
+        min = cache.poloniex[sym];
+    }
+    return min;
+}
 
-app.get('/api/ethereum', (req, res) => {
-    //querry db to find best exchange rate for ethereum
-    res.send();
+app.get('/huey', (req, res) => {
+    //return best exchange rate for ethereum from cache
+    //let best_rate = getMinRate(exchangeRateCache,"eth");
+    res.send(exchangeRateCache);
 });
-app.get('/api/litecoin', (req, res) => {
-    //querry db to find best exchange rate for litecoin
-    res.send();
+app.get('/luey', (req, res) => {
+    //return best exchange rate for litecoin from cache
+    res.send(exchangeRateCache);
 });
-app.get('/api/dash', (req, res) => {
-    //querry db to find best exchange rate for dash 
-    res.send();
+app.get('/duey', (req, res) => {
+    //return best exchange rate for dash from cache
+    res.send(exchangeRateCache);
 });
 
-app.get('/api/kraken', (req, res) => res.send({ exchange: 'kraken' }));
-app.get('/api/bittrex', (req, res) => res.send({ exchange: 'bittrex' }));
-app.get('/api/binance', (req, res) => res.send({ exchange: 'binance' }));
-app.get('/api/poloniex', (req, res) => res.send({ exchange: 'poloniex' }));
-app.get('/api/coincap', (req, res) => res.send({ exchange: 'coincap' }));
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public/index.html'));
 });
 
-app.listen(3232, () => console.log('Listening on port 3232!!'));
+app.listen(3232, () => console.log('Donalds Present server listening on port 3232!!'));
